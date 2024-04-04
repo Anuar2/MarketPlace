266e397cbc597ed8df79a69ffb1161bc269454a3
Anoir <orazbekov.anvar2@gmail.com>
2024-04-04 22:12:37 +0500

    [chore]: set folder

---
+++ MarketPlace/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MarketPlace
+//
+//  Created by Anuar Orazbekov on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
