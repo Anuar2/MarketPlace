936e1a2ffed49a07b80730b9a61e16e1f34d94bc
Anoir <orazbekov.anvar2@gmail.com>
2024-04-05 15:00:02 +0500

    [add]: set view

---
+++ MarketPlace/Modules/View/BaseSegmentControl.swift
@@ -0,0 +1,55 @@
+//
+//  BaseSegmentControl.swift
+//  MarketPlace
+//
+//  Created by Anuar Orazbekov on 05.04.2024.
+//
+
+import UIKit
+
+final class BaseSegmentControl: UISegmentedControl {
+    
+    init(items: [String]) {
+        super.init(items: items)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
+        layer.masksToBounds = true
+        
+        let selectedImageViewIndex = numberOfSegments
+        if let selectedImageView = subviews[selectedImageViewIndex] as? UIImageView {
+            selectedImageView.backgroundColor = .yellow
+            selectedImageView.image = nil
+            selectedImageView.bounds = selectedImageView.bounds.insetBy(dx: 2.5, dy: 2.5)
+            selectedImageView.layer.masksToBounds = true
+            selectedImageView.layer.cornerRadius = bounds.height / 2
+            selectedImageView.layer.removeAnimation(forKey: "SelectionBounds")
+        }
+    }
+    
+    func configureView() {
+        selectedSegmentIndex = 0
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            backgroundColor = .gray
+            setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+            setTitleTextAttributes([.foregroundColor: UIColor.lightGray], for: .normal)
+        } else {
+            backgroundColor = .lightGray
+            setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+            setTitleTextAttributes([.foregroundColor: UIColor.darkGray], for: .normal)
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+         super.traitCollectionDidChange(previousTraitCollection)
+         configureView()
+     }
+}
---
+++ MarketPlace/Modules/View/CocktailsView.swift
@@ -0,0 +1,66 @@
+//
+//  CocktailsView.swift
+//  MarketPlace
+//
+//  Created by Anuar Orazbekov on 05.04.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class CardListView: UIImageView {
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 16.0, weight: .regular)
+        return label
+    }()
+    
+    enum Constant {
+        enum Spacing {
+            static var space_8: CGFloat = 8.0
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        configureLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        addSubview(label)
+    }
+    
+    private func configureLayouts() {
+        label.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(Constant.Spacing.space_8)
+        }
+    }
+    
+    func setTitle(_ title: String?) {
+        label.text = title
+        updateAppearance()
+    }
+    
+    private func updateAppearance() {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            label.textColor = .white
+            backgroundColor = .darkGray
+        } else {
+            label.textColor = .black
+            backgroundColor = .lightGray
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateAppearance()
+    }
+}
